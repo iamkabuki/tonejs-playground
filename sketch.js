45ec82ce15fd051e0d4ebc0417dd24c7dcf7d593
Jan Hennig <JH@jans-mbp.speedport_w_724v_09011603_06_008>
2022-01-12 00:01:43 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,51 +1,21 @@
-const TAU = 6.28;
-
-let red, green, blue;
-let numDots;
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+const synth = new Tone.Synth().toMaster();
 
 function setup() {
-  red = 255 * fxrand();
-  green = 255 * fxrand();
-  blue = 255 * fxrand();
-  
-  numDots = Math.floor(fxrand() * 10);
+  createCanvas(400, 400);
 
-  createCanvas(windowWidth, windowHeight);
-
-  window.$fxhashFeatures = {
-    "Red Value": red,
-    "Green Value": green,
-    "Blue Value": blue,
-    "Number of Dots": numDots,
-  };
 }
 
 function draw() {
-    
-    const synth = new Tone.Synth().toMaster()
-synth.triggerAttackRelease('C4', '8n')
+  background(0);
+  
 
-document.getElementById("play-button").addEventListener("click", function() {
-  if (Tone.Transport.state !== 'started') {
-    Tone.Transport.start();
-  } else {
-    Tone.Transport.stop();
-  }
-});
-    
-//  background(red, green, blue);
-//
-//  var h4 = windowHeight / 4;
-//  var h2 = windowHeight / 2;
-//  var w2 = windowWidth / 2;
-//  var timer = frameCount * 0.05;
-//  
-//  for (var i = 0; i < numDots; i++) {
-//    var pct = i / numDots;
-//    ellipse(w2 + sin(timer + TAU * pct) * h4, h2 + cos(timer + TAU * pct) * h4, 80, 80);
-  }
+
+
+}
+
+function mouseClicked() {
+  
+  synth.triggerAttackRelease("C4", "8n");
+
+  
 }
